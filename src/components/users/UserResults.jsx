57952619c944a407d755944e9cd3dd477e624b80
Pavel Mezencev <mezencev24@mail.ru>
2022-10-10 17:45:28 +0300

    add user item and spinner component

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from 'react';
+import UserItem from './UserItem';
+import Spinner from '../layout/Spinner';
 
 const UserResults = () => {
   const [users, setUsers] = useState([]);
@@ -14,9 +16,9 @@ const UserResults = () => {
       const response = await fetch(
         `${process.env.REACT_APP_GITHUB_URL}/users`,
         {
-          headers: {
-            Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-          },
+          // headers: {
+          //   Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+          // },
         }
       );
       const users = await response.json();
@@ -28,13 +30,13 @@ const UserResults = () => {
   };
 
   if (loading) {
-    return <h3>Loading...</h3>;
+    return <Spinner />;
   }
 
   return (
     <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
       {users.map((user) => {
-        return <h3 key={user.id}>{user.login}</h3>;
+        return <UserItem key={user.id} user={user} />;
       })}
     </div>
   );
