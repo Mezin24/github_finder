421490e41801026de1c0b03b9396719d6c3b5ab0
Pavel Mezencev <mezencev24@mail.ru>
2022-10-11 11:52:51 +0300

    add user page ui

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -1,16 +1,177 @@
 import { useGithubContext } from '../context/github_context/GithubContext';
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { FaCodepen, FaStore, FaUserFriends, FaUsers } from 'react-icons/fa';
+import Spinner from '../components/layout/Spinner';
+import { Link } from 'react-router-dom';
 
 const User = () => {
-  const { getUser, user } = useGithubContext();
+  const { getUser, user, loading } = useGithubContext();
   const params = useParams();
 
   useEffect(() => {
     getUser(params.login);
   }, [getUser, params.login]);
 
-  return <div>{user.login}</div>;
+  const {
+    avatar_url,
+    blog,
+    bio,
+    company,
+    created_at,
+    email,
+    events_url,
+    followers,
+    followers_url,
+    following,
+    following_url,
+    gists_url,
+    gravatar_id,
+    hireable,
+    html_url,
+    id,
+    location,
+    login,
+    name,
+    node_id,
+    organizations_url,
+    public_gists,
+    public_repos,
+    received_events_url,
+    repos_url,
+    site_admin,
+    starred_url,
+    subscriptions_url,
+    twitter_username,
+    type,
+    updated_at,
+    url,
+  } = user;
+
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <>
+      <div className='w-full mx-auto lg:w-10/12'>
+        <div className='mb-4'>
+          <Link className='btn btn-ghost' to='/'>
+            Back To Search
+          </Link>
+        </div>
+        <div className='grid grid-cols-1 xl:grid-cols-3 lg:grid-cols-3 md:grid-cols-3 mb-8 md:gap-8'>
+          <div className='custom-card-image mb-6 md:mb-0'>
+            <div className='rounded-lg shadow-xl card image-full'>
+              <figure>
+                <img src={avatar_url} alt={login} />
+              </figure>
+              <div className='card-body  justify-end '>
+                <h2 className='card-title mb-0 text-white'>{name}</h2>
+                <p className='flex-grow-0 text-white'>{login}</p>
+              </div>
+            </div>
+          </div>
+          <div className='col-span-2'>
+            <div className='mb-6'>
+              <h1 className='text-3xl card-title'>
+                {name}
+                <div className='ml-2 mr-1 badge badge-success'>{type}</div>
+                {hireable && (
+                  <div className='mx-1 badge badge-info'>Hireble</div>
+                )}
+              </h1>
+              <p>{bio}</p>
+              <div className='mt-4 card-actions'>
+                <a
+                  href={html_url}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='btn btn-outline'
+                >
+                  Visit Github Profile
+                </a>
+              </div>
+            </div>
+            <div className='w-full rounded-lg shadow-md bg-base-100 stats'>
+              {location && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Location</div>
+                  <div className='text-lg stat-value'>{location}</div>
+                </div>
+              )}
+              {blog && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Website</div>
+                  <div className='text-lg stat-value'>
+                    <a
+                      href={`https://${blog}`}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {blog}
+                    </a>
+                  </div>
+                </div>
+              )}
+              {twitter_username && (
+                <div className='stat'>
+                  <div className='stat-title text-md'>Twitter</div>
+                  <div className='text-lg stat-value'>
+                    <a
+                      href={`https://twitter.com/${twitter_username}`}
+                      target='_blank'
+                      rel='noreferrer'
+                    >
+                      {twitter_username}
+                    </a>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+        <div className='w-full py-5 mb-6 rounded-lg shadow-md bg-base-100 stats'>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUsers className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Followers</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {followers}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaUserFriends className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Following</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {following}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaCodepen className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Public Repos</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {public_repos}
+            </div>
+          </div>
+          <div className='stat'>
+            <div className='stat-figure text-secondary'>
+              <FaStore className='text-3xl md:text-5xl' />
+            </div>
+            <div className='stat-title pr-5'>Public Gists</div>
+            <div className='stat-value pr-5 text-3xl md:text-4xl'>
+              {public_gists}
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default User;
